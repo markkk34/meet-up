7ce964e8794f7ccdb2fc3ea234d3890e0a858ff3
markkk34 <mark.pochtarev@mail.ru>
2022-04-09 17:39:53 +0400

    Made Adding-cart layout

--- src/components/layout/meetups/NewMeetupForm.js
+++ src/components/layout/meetups/NewMeetupForm.js
@@ -2,12 +2,33 @@ import Card from "../ui/Card";
 import NewMeetupFormStyles from './NewMeetupForm.module.css';
 
 const NewMeetupForm = () => {
+
+    function submitHandler(event)
+    {
+        event.preventDefault();
+    }
+
     return (
         <Card>
-            <form className={NewMeetupFormStyles.form}>
+            <form className={NewMeetupFormStyles.form} onSubmit={submitHandler}>
                 <div className={NewMeetupFormStyles.control}>
-                    <label htmlFor='title'></label>
-                    <input type='text' required id='title'></input>
+                    <label htmlFor='title'>Title</label>
+                    <input type='text' className = {NewMeetupFormStyles.title} required id='title'></input>
+                </div>
+                <div className={NewMeetupFormStyles.control}>
+                    <label htmlFor='image'>Image url</label>
+                    <input type='url' required id='image'></input>
+                </div>
+                <div className={NewMeetupFormStyles.control}>
+                    <label htmlFor='address'>Address</label>
+                    <input type='address' required id='address'></input>
+                </div>
+                <div className={NewMeetupFormStyles.control}>
+                    <label htmlFor='description'>Description</label>
+                    <textarea className = {NewMeetupFormStyles.title} required rows = '5' id='description'/>
+                </div>
+                <div className={NewMeetupFormStyles.control}>
+                    <button className='add'>Add Meet Up</button>
                 </div>
             </form>
         </Card>
